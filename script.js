38752af4dca149b509ccbef9c2c94a65eb227511
NickSessa <nicholas.sessa@gmail.com>
2021-02-18 17:00:03 -0500

    Fixed a problem with duplicate images

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    //console.log(toggleSwitch.children);
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+    image1.src = 'img/undraw_proud_coder_dark.svg';
+    image2.src = 'img/undraw_feeling_proud_dark.svg';
+    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    //console.log(event.target.checked);
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
